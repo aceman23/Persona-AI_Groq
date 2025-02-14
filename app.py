056cfa18fb4b3a6cd8b1ab8a12f01b6f457d1ba3
Anton Ansalmar <89657802+aceman23@users.noreply.github.com>
2025-02-14 11:00:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ df = pd.read_csv('persona.csv')
 unique_categories_list  = df["Category"].unique().tolist()
 
 categories_list = ["All",*unique_categories_list]
-selected = pills("Category: ", categories_list,['💻','🐧','👨‍💻','💻','🔒','🔧','📡','💽','🤖'])
+selected = pills("Category: ", categories_list,['💻','🐧','👨‍💻','💻','🔒','🔧','📡','💽'])
 
 row1 = st.columns(4) 
 row2 =  st.columns(4) 
