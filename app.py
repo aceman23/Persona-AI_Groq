263b84f4d91c1ea7df0fe2194d547e2ea4009956
Anton Ansalmar <89657802+aceman23@users.noreply.github.com>
2025-09-05 12:56:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,19 @@ from streamlit_pills import pills
 from share_state import set_server_state
 from util import hide_header_footer
 
-st.set_page_config(page_title="Multi-IT Persona Gen-AI Demo",layout='wide', initial_sidebar_state='collapsed')
-
-hide_header_footer()
-st.markdown("""
-            <style>
-            div[data-testid="collapsedControl"]{
-                    visibility: collapse;
-            }
-            section[data-testid="stSidebar"]{
-                   visibility: collapse;
-            }
-            </style>
-            """,unsafe_allow_html=True)
-
+st.set_page_config(page_title="Multi-IT Persona Gen-AI Demo", layout='wide', initial_sidebar_state='expanded')
+# The following lines are no longer needed.
+# hide_header_footer()
+# st.markdown("""
+#             <style>
+#             div[data-testid="collapsedControl"]{
+#                     visibility: collapse;
+#             }
+#             section[data-testid="stSidebar"]{
+#                    visibility: collapse;
+#             }
+#             </style>
+#             """,unsafe_allow_html=True)
 
 
 def open_chat(id,role,prompt,description):
@@ -25,6 +24,7 @@ def open_chat(id,role,prompt,description):
     set_server_state("persona_role", role)
     set_server_state("persona_prompt", prompt)
     set_server_state("description", description)
+    st.switch_page('pages/chat.py')
 
 def persona_grid_cell(id,role,description,prompt):
     st.markdown(f"**ID: :** psn_id_{id}{role[:4]}")
@@ -32,8 +32,8 @@ def persona_grid_cell(id,role,description,prompt):
     st.markdown(f"**Description:** {description}")
     
     if st.button('💬 Chat Now',key=f"psn_id_{id}{role[:4]}",on_click=open_chat,args=(id,role,prompt,description)):
-         st.switch_page('pages/chat.py')
-         
+        st.switch_page('pages/chat.py')
+        
 
 st.header("Multi-IT Persona Gen-AI Demo", divider='rainbow')
 st.caption(" Leveraging the power of Generative AI and Groq Cloud to create virtual IT personas. These AI-powered assistants can answer questions, troubleshoot issues, and provide support across various IT domains, enhancing efficiency and user satisfaction within your enterprise.")
@@ -55,7 +55,7 @@ row5 = st.columns(4)
 
 grid  = [ cell.container(border=True) for cell in row1+row2+row3+row4+row5]
 
-if selected != "All":   
+if selected != "All":    
     df =  df[df['Category'] == format(selected)]
 
 i = 0
@@ -63,4 +63,3 @@ for index,cell in df.iterrows():
     with grid[i]:
         persona_grid_cell(cell["Id"],cell["Role"],cell["Description"],cell["Prompt"])
     i = i + 1
-
