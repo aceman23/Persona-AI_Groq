5910205a5c25b7bfd25caf5b5ff887bcdd0ada25
Anton Ansalmar <89657802+aceman23@users.noreply.github.com>
2025-09-05 10:59:02 -0700

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -49,7 +49,7 @@ st.markdown("""
             """,unsafe_allow_html=True)
 
 client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
+    api_key=os.environ.get("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"),
 )
     
 user_input = st.chat_input()
